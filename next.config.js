cf59710d7db0683b69192448adb6039999edcecf
DataCodeLiteracy <dataliteracy@icloud.com>
2025-09-02 08:46:05 +0900

    [ 🔧 | | fix ] build 오류 해결
    2025.09.02

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  experimental: {
+    // NFT 파일 생성 비활성화
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
